6e534cd0aad70d9da5863e64b487139e38b4932a
Khandaker Sajjad Hossen <sk.sajjad4207@gmail.com>
2022-08-04 22:49:58 +0600

    daisyui added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
-import FirstBanner from './components/First_banner/FirstBanner';
 
 function App() {
   return (
     <div className="App">
-      <FirstBanner/>
+     
     </div>
   );
 }
--- src/components/First_banner/FirstBanner.js
+++ src/components/First_banner/FirstBanner.js
@@ -2,9 +2,7 @@ import React from 'react';
 
 const FirstBanner = () => {
     return (
-        <h1 className="text-3xl font-bold underline bg-black text-white">
-        Hello world!
-      </h1>
+      <button class="btn btn-active btn-secondary">Button</button>
     );
 };
 
