d9170c5805eea48f58b56a3df36abaed0eb2f7b1
Khandaker Sajjad Hossen <sk.sajjad4207@gmail.com>
2022-08-04 16:20:47 +0600

    tailwind css added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import FirstBanner from './components/First_banner/FirstBanner';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <FirstBanner/>
     </div>
   );
 }
---
+++ src/components/First_banner/FirstBanner.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const FirstBanner = () => {
+    return (
+        <h1 className="text-3xl font-bold underline bg-black text-white">
+        Hello world!
+      </h1>
+    );
+};
+
+export default FirstBanner;
